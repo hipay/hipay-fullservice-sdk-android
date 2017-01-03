4e904ac5373603b2c8291fb138fa83842129955c
Nicolas FILLION <nfillion21@gmail.com>
2017-01-03 11:29:44 +0100

    Add payment card storage option in config

--- hipayfullservice-android/hipayfullservice/src/main/java/com/hipay/fullservice/screen/activity/PaymentScreenActivity.java
+++ hipayfullservice-android/hipayfullservice/src/main/java/com/hipay/fullservice/screen/activity/PaymentScreenActivity.java
@@ -3,6 +3,7 @@ package com.hipay.fullservice.screen.activity;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 
+import com.hipay.fullservice.core.client.config.ClientConfig;
 import com.hipay.fullservice.core.requests.order.PaymentPageRequest;
 import com.hipay.fullservice.screen.model.CustomTheme;
 
@@ -14,10 +15,8 @@ public abstract class PaymentScreenActivity extends AppCompatActivity {
 
     public static void start(Activity activity, PaymentPageRequest paymentPageRequest, String signature, CustomTheme theme) {
 
-        boolean isCardStorageEnabled = true;
-        // check if we got some in database
-
-        if (isCardStorageEnabled)
+        //TODO we also need to check if we have so paymentCardToken available
+        if (ClientConfig.getInstance().isPaymentCardStorageEnabled())
         {
             PaymentCardsActivity.start(activity, paymentPageRequest, signature, theme);
 
