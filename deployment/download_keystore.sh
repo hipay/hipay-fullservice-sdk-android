ee037efdeb2cabb5c927aed43694bcc8c67a6688
Morgan <mbaumard@hipay.com>
2019-03-15 10:20:27 +0100

    debug bash

--- deployment/download_keystore.sh
+++ deployment/download_keystore.sh
@@ -4,6 +4,7 @@ if [[ $KEYSTORE && ${KEYSTORE} && $KEYSTORE_URI && ${KEYSTORE_URI} ]]
 then
     echo "Keystore detected - downloading..."
     curl -L -o ${KEYSTORE} ${KEYSTORE_URI}
+    cat ${KEYSTORE}
 else
     echo "Keystore uri not set.  .APK artifact will not be signed."
 fi
